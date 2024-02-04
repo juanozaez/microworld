0586b4e3e17cc8edd659e8e586f46186f4066b61
juanlu <juan.ozaez@code.seat>
2024-02-04 16:54:04 +0100

    Initial commit

---
+++ src/test/kotlin/com/homelab/microworld/MicroworldApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.homelab.microworld
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MicroworldApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
