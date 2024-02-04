1d09f6a563fc32d2dbb0901ed51025fed6aa06ab
juanlu <juan.ozaez@code.seat>
2024-02-04 18:05:31 +0100

    scaffolding

--- context/user/src/main/kotlin/com/microworld/user/infra/in/register/RegisterUserController.kt
+++ context/user/src/main/kotlin/com/microworld/user/infra/in/register/RegisterUserController.kt
@@ -2,16 +2,35 @@ package com.microworld.user.infra.`in`.register
 
 import com.microworld.user.application.register.CreateUserCommandHandler
 import com.microworld.user.application.register.RegisterUserCommand
+import jakarta.annotation.PostConstruct
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
+
 
 @RestController
 class RegisterUserController(private val commandHandler: CreateUserCommandHandler) {
 
+    @PostConstruct
+    fun hola(){
+        println("Hola2")
+    }
+
     @PostMapping("/users")
     fun register(@RequestBody body: RegisterUserBody) {
         commandHandler.handle(RegisterUserCommand(body.name, body.surname))
+    }
+
+    @GetMapping("/users")
+    fun register() = "hola"
+
+    @RequestMapping("/")
+    @ResponseBody
+    fun greeting(): String {
+        return "Hello, World"
     }
 }
 
--- src/test/kotlin/com/homelab/microworld/context/user/register/RegisterUserAcceptanceTest.kt
+++ src/test/kotlin/com/homelab/microworld/context/user/register/RegisterUserAcceptanceTest.kt
@@ -1,31 +1,58 @@
 package com.homelab.microworld.context.user.register
 
-import com.microworld.user.domain.UserRepository
+import io.restassured.RestAssured
+import io.restassured.module.kotlin.extensions.Extract
 import io.restassured.module.kotlin.extensions.Given
 import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
-import jakarta.inject.Inject
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.server.LocalServerPort
 
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RegisterUserAcceptanceTest {
+
+    @LocalServerPort
+    private val port = 0
 
     //@Inject
     //private lateinit var repository: UserRepository
 
+    @Autowired
+    private val restTemplate: TestRestTemplate? = null
+
+    @Test
+    @Throws(Exception::class)
+    fun greetingShouldReturnDefaultMessage() {
+        assertThat(
+            restTemplate!!.getForObject(
+                "http://localhost:$port/",
+                String::class.java
+            )
+        ).contains("Hello, World")
+    }
+
     @Test
     fun `registers an user`() {
-        Given {
-            body(payload)
-        }
+        RestAssured.port = port
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails()
+
+        //Given {
+         //   body(payload)
+        //}
         When {
-            post("/users")
+            post("/")
         } Then {
-      //      Assertions.assertEquals(1, repository.findAll().size)
+            statusCode(200)
+        } Extract {
+            val body = body().asString()
+            Assertions.assertEquals(body, "Hello, World")
+
         }
 
     }
